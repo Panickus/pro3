6de5e2e5262ca424b30ad3a8c9eb4cf87bc9d3df
panickus <felipedg@outlook.es>
2024-01-08 09:00:03 +0100

    init one

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+// Crear el elemento del encabezado
+const header = document.createElement('header');
+
+// Agregar contenido al encabezado
+header.innerHTML = '<h1>Mi Página</h1>';
+
+// Agregar el encabezado al cuerpo del documento
+document.body.appendChild(header);
